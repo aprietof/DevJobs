52b41158ea446246cde5e2a32d8b16718df10fc4
aprietof <adrianprieto00@gmail.com>
2016-08-15 18:48:00 -0400

    welcome controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
