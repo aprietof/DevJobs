aa4931ef35e5e565560ecd767fc28a10de71e2f1
aprietof <adrianprieto00@gmail.com>
2016-08-15 22:20:53 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Job.create(
+ title: "Senior Web Developer",
+ description: "This position is for a strong Full Stack developer who would like to drive their own growth inside of our company. You will have a lot of agency (and responsibility!) from the outset, and you will be expected to run projects from start to finish, interface with clients, and oversee more junior developers",
+ company: "Dom & Tom",
+ url: "http://domandtom.applytojob.com/",
+ location: "New York",
+)
+
+Job.create(
+ title: "Junior Web Developer",
+ description: "This position is for a junior Full Stack developer who would like to drive their own growth inside of our company. You will have a lot of agency (and responsibility!) from the outset, and you will be expected to run projects from start to finish, interface with clients, and oversee more junior developers",
+ company: "Tom & Jerry",
+ url: "http://tomandjerry.applytojob.com/",
+ location: "San Francisco",
+)
+
+Job.create(
+ title: "IOS Developer",
+ description: "This position is for a IOS developer who would like to drive their own growth inside of our company. You will have a lot of agency (and responsibility!) from the outset, and you will be expected to run projects from start to finish, interface with clients, and oversee more junior developers",
+ company: "A-Team",
+ url: "http://ateam.applytojob.com/",
+ location: "Houston",
+)
+
+Job.create(
+ title: "Junior Java Developer",
+ description: "This position is for a junior java developer who would like to drive their own growth inside of our company. You will have a lot of agency (and responsibility!) from the outset, and you will be expected to run projects from start to finish, interface with clients, and oversee more junior developers",
+ company: "Google",
+ url: "http://google.applytojob.com/",
+ location: "New York",
+)
+
+Job.create(
+ title: "Senior Web Developer",
+ description: "This position is for a strong Full Stack developer who would like to drive their own growth inside of our company. You will have a lot of agency (and responsibility!) from the outset, and you will be expected to run projects from start to finish, interface with clients, and oversee more junior developers",
+ company: "Dom & Tom",
+ url: "http://domandtom.applytojob.com/",
+ location: "New York",
+)
