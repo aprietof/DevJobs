27cc1724be97883e0fe3b194e868b478ffb0ad7c
aprietof <adrianprieto00@gmail.com>
2016-08-17 22:04:00 -0400

    athorization policy added for job

--- app/policies/job_policy.rb
+++ app/policies/job_policy.rb
@@ -1,5 +1,25 @@
 class JobPolicy < ApplicationPolicy
 
+  def created_by_company?
+    user.company? && record.user_id == user.id
+  end
 
+
+  def create?
+    user.admin? || created_by_company?
+  end
+
+  def new?
+    user.admin? || created_by_company?
+  end
+
+  def update?
+    user.admin? || created_by_company?
+  end
+
+
+  def destroy?
+    user.admin? || created_by_company?
+  end
 
 end
