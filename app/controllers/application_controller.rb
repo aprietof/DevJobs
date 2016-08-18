c92bb7261d66d71156e4a603e2316b9d547a6de5
aprietof <adrianprieto00@gmail.com>
2016-08-17 21:00:36 -0400

    Added company role checkbox to devise signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   include Pundit
-  
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def after_sign_in_path_for(resource)
@@ -21,4 +22,10 @@ class ApplicationController < ActionController::Base
   # def after_sign_out_path_for(resource_or_scope)
   #   new_user_session_path
   # end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+  end
+
 end
