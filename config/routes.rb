02512452cf2367772721cfc6a308337285614670
aprietof <adrianprieto00@gmail.com>
2016-08-18 17:58:22 -0400

    added nested routes for users-job-applications

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "jobs#index"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  resources :users do
+    # nested resource for job_applications
+    resources :job_applications, only: [:show, :index]
+  end
+
   resources :jobs, :categories, :job_applications
 end
