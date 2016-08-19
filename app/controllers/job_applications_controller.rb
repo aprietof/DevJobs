76125ff936315f6da4a787c1263646e131f0fd5b
aprietof <adrianprieto00@gmail.com>
2016-08-18 20:34:25 -0400

    job application index nested routes views

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -2,6 +2,14 @@ class JobApplicationsController < ApplicationController
   # before_action :set_job_application, :except => [:index]
 
   def index
+    @items_per_page = 9
+    @latest_jobs = Job.all_sort_by_date_skip_first
+
+    if params[:user_id]
+      @job_applications = User.find(params[:user_id]).job_applications.order_and_paginated(params[:page], @items_per_page)
+    else
+      @job_applications = JobApplication.order_and_paginated(params[:page], @items_per_page)
+    end
   end
 
   def new
@@ -11,7 +19,7 @@ class JobApplicationsController < ApplicationController
   def create
     @job_application = JobApplication.find_by_user_id_and_job_id(params[:job_application][:user_id], params[:job_application][:job_id])
     if !@job_application.nil?
-      redirect_to :back, alert: "You already applied for this job."
+      redirect_to root_path, alert: "You already applied for this job."
     else
       @job_application = JobApplication.new(job_application_params)
       if @job_application.save
--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,4 +1,7 @@
 class JobApplication < ApplicationRecord
+  include Sortable::InstanceMethods
+  extend Sortable::ClassMethods
+  
   belongs_to :job
   belongs_to :applicant, :class_name => 'User', :foreign_key => 'user_id'
 end
