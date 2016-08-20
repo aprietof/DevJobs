74d35c1cda9aa014b64cafb7261a8401df9839f8
aprietof <adrianprieto00@gmail.com>
2016-08-20 03:31:51 -0400

    fixed job policy

--- app/policies/job_policy.rb
+++ app/policies/job_policy.rb
@@ -1,22 +1,16 @@
 class JobPolicy < ApplicationPolicy
 
   def created_by_company?
-    user.company? && record.user_id == user.id
+    user.company? && record.company_id == user.id
   end
-
 
   def create?
-    user.admin? || created_by_company?
-  end
-
-  def new?
-    user.admin? || created_by_company?
+    user.admin? || user.company?
   end
 
   def update?
     user.admin? || created_by_company?
   end
-
 
   def destroy?
     user.admin? || created_by_company?
