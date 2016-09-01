626c00b43c92721ddcfa00d4e107b794e3a6d3b5
aprietof <adrianprieto00@gmail.com>
2016-09-01 14:45:23 -0400

    :dependent => :destroy added to job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,7 +5,7 @@ class Job < ApplicationRecord
   validates_presence_of :title, :description, :company_name, :location, :category_id
   belongs_to :company, :class_name => 'User', :foreign_key => 'company_id'
   belongs_to :category
-  has_many :job_applications
+  has_many :job_applications, :dependent => :destroy
   has_many :applicants, :through => :job_applications, :class_name => 'User'
   has_many :jobs_skills
   has_many :skills, :through => :jobs_skills
