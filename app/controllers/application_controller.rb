c598b37f8657300be3a7aa11e8888c68043fefde
aprietof <adrianprieto00@gmail.com>
2016-09-04 23:12:24 -0400

    :authenticate_user! added to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
-
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
