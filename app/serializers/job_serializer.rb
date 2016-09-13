992a334f82a303e39462c120118e61cc7e133a90
aprietof <adrianprieto00@gmail.com>
2016-09-12 20:26:43 -0400

    job serializer edited

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,3 +1,15 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :description, :company_name, :url,
+   :location, :created_at, :company_id, :created_at
+  has_one :category
 end
+
+# t.string   "title"
+# t.text     "description"
+# t.string   "company_name"
+# t.string   "url"
+# t.string   "location"
+# t.datetime "created_at",   null: false
+# t.datetime "updated_at",   null: false
+# t.integer  "category_id"
+# t.integer  "company_id"
