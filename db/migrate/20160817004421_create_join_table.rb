d2ce7e443d299b87c30c094b996061898ccc541f
aprietof <adrianprieto00@gmail.com>
2016-10-13 08:16:55 -0400

    conflicting migration deleted

--- db/migrate/20160817004421_create_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTable < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :jobs, :skills do |t|
-      t.index [:job_id, :skill_id]
-      # t.index [:_id, :_id]
-    end
-  end
-end
